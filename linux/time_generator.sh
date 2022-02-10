c2a3483783ea8e66ca24954a80cb24a852edc163
KirillRizhikov <rizhikovkirill@gmail.com>
2022-02-10 22:12:21 +0200

    Time generator and weather generator were added

---
+++ linux/time_generator.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+echo "The date and time are: $(date +%m/%d/%Y) - $(date +%T)" >> /home/kirill/PycharmProjects/python_education/linux/time_generator_log.txt
---
+++ linux/weather_generator.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+curl -o /home/kirill/PycharmProjects/python_education/linux/weather.txt "https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=2b1d8d815396598f8e6127dd788c8c4f"
